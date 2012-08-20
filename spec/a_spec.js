da32c45dc0f50725634f4abe0e86c4fdae28a5a8
tomaszjureczko <tomasz.jureczko@gmail.com>
2012-08-20 19:41:21 +0100

    Implemented playing on the top-left corner for the first move.

--- spec/a_spec.js
+++ spec/a_spec.js
@@ -1,7 +1,18 @@
-var board, empty;
+var board, empty,
+
+    TicTacToe = {
+        play: function() {
+            board = [['x'], [], []];
+        }
+    };
 
 describe("noughts and crosses", function() {
     it("starts with an empty board", function() {
         expect(board).toBe(empty);
+    });
+
+    it("puts the cross on top left position", function() {
+        TicTacToe.play('x',0,0);
+        expect(board).toEqual([['x'],[],[]]);
     });
 });

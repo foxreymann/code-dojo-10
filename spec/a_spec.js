61a87d9af4711a408efd911ef13c6a8c1447ea5e
tomaszjureczko <tomasz.jureczko@gmail.com>
2012-08-20 19:43:58 +0100

    Moved the board into the TicTacToe function.

--- spec/a_spec.js
+++ spec/a_spec.js
@@ -1,20 +1,25 @@
-var board, empty,
+var empty,
 
     TicTacToe = function() {
+        var board;
+
         return {
             play: function() {
                 board = [['x'], [], []];
-            }
+            },
+            board: function() { return board; }
         };
     };
 
 describe("noughts and crosses", function() {
     it("starts with an empty board", function() {
-        expect(board).toBe(empty);
+        var ticTacToe = TicTacToe();
+        expect(ticTacToe.board()).toBe(empty);
     });
 
     it("puts the cross on top left position", function() {
-        TicTacToe().play('x',0,0);
-        expect(board).toEqual([['x'],[],[]]);
+        var ticTacToe = TicTacToe();
+        ticTacToe.play('x', 0, 0);
+        expect(ticTacToe.board()).toEqual([['x'], [], []]);
     });
 });
